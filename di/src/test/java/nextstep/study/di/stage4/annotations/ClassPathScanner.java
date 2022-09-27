28ffe6fc66bfbb489ba0a1bafc2cc5f2f3c9e83b
skullKim <kyk990328@gmail.com>
2022-09-27 23:01:20 +0900

    test: stage4 완료

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,20 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        final Reflections reflections = new Reflections(packageName);
+        return Stream.of(
+                        reflections.getTypesAnnotatedWith(Service.class),
+                        reflections.getTypesAnnotatedWith(Repository.class)
+                )
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
     }
 }
--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContext.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContext.java
@@ -1,6 +1,10 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +14,69 @@ class DIContext {
     private final Set<Object> beans;
 
     public DIContext(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(this::createInstance)
+                .collect(Collectors.toSet());
+        injectDependency();
+    }
+
+
+    private Object createInstance(final Class<?> clazz) {
+        try {
+            final Constructor<?> declaredConstructor = clazz.getDeclaredConstructor();
+            declaredConstructor.setAccessible(true);
+            return declaredConstructor.newInstance();
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    private void injectDependency() {
+        for (Object bean : beans) {
+            try {
+                findBeanAndInject(bean);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+    }
+
+    private void findBeanAndInject(final Object bean) throws IllegalAccessException {
+        Field[] fields = bean.getClass()
+                .getDeclaredFields();
+        for (Field field : fields) {
+            final Class<?> type = field.getType();
+            for (Object bb : beans) {
+                System.out.println(bb);
+            }
+            beans.stream()
+                    .filter(b -> type.isAssignableFrom(b.getClass())
+                            && field.isAnnotationPresent(Inject.class))
+                    .findFirst()
+                    .ifPresent(value -> setField(field, bean, value));
+        }
+    }
+
+    private void setField(final Field field, final Object target, final Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(target, value);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 
     public static DIContext createContextForPackage(final String rootPackageName) {
-        return null;
+        final Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        assert classes != null;
+        return new DIContext(classes);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> bean.getClass().getName().equals(aClass.getName()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
