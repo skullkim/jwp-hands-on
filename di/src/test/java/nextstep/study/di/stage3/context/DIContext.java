3d0f98787c71d96a6cf403b0c36756fad4696f9f
skullKim <kyk990328@gmail.com>
2022-09-27 22:35:50 +0900

    test: stage3 test 완료

--- di/src/test/java/nextstep/study/di/stage3/context/DIContext.java
+++ di/src/test/java/nextstep/study/di/stage3/context/DIContext.java
@@ -1,6 +1,10 @@
 package nextstep.study.di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +14,58 @@ class DIContext {
     private final Set<Object> beans;
 
     public DIContext(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(this::createInstance)
+                .collect(Collectors.toSet());
+        injectDependency();
+    }
+
+    private Object createInstance(final Class<?> clazz) {
+        try {
+            final Constructor<?> declaredConstructor = clazz.getDeclaredConstructor();
+            declaredConstructor.setAccessible(true);
+            return declaredConstructor.newInstance();
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    private void injectDependency() {
+        for (Object bean : beans) {
+            try {
+                findBeanAndInject(bean);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+    }
+
+    private void findBeanAndInject(final Object bean) throws IllegalAccessException {
+        Field[] fields = bean.getClass()
+                .getDeclaredFields();
+        for (Field field : fields) {
+            final Class<?> type = field.getType();
+            beans.stream()
+                    .filter(b -> type.isAssignableFrom(b.getClass()))
+                    .findFirst()
+                    .ifPresent(value -> setField(field, bean, value));
+        }
+    }
+
+    private void setField(final Field field, final Object target, final Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(target, value);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> bean.getClass().getName().equals(aClass.getName()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
